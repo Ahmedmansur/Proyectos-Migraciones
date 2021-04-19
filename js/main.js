f4b81a6117ceab0087ec8300561fccc82fb5a164
Ahmedmansur <joelakaahmedfabregas@gmail.com>
2021-04-19 13:21:26 +0200

    Entrevistas/mhic/grupos editado

--- js/main.js
+++ js/main.js
@@ -20,3 +20,25 @@ $(document).ready(function () {
         $('.hover_bkgr_fricc1').hide();
     });
   });
+  $(document).ready(function () {
+     $('.trigger_popup_fricc2').click(function(){
+        $('.hover_bkgr_fricc2').show();
+     });
+     $('.hover_bkgr_fricc2').click(function(){
+         $('.hover_bkgr_fricc2').hide();
+     });
+     $('.popupCloseButton2').click(function(){
+         $('.hover_bkgr_fricc2').hide();
+     });
+   });
+   $(document).ready(function () {
+      $('.trigger_popup_fricc3').click(function(){
+         $('.hover_bkgr_fricc3').show();
+      });
+      $('.hover_bkgr_fricc3').click(function(){
+          $('.hover_bkgr_fricc3').hide();
+      });
+      $('.popupCloseButton3').click(function(){
+          $('.hover_bkgr_fricc3').hide();
+      });
+    });
